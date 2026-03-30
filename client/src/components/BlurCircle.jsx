5b7828e94292aa164ec7d23b2b0555cb69a327d6
vaibhav1-prog <baishkhiyarv@gmail.com>
2026-03-30 14:00:27 +0530

    Secrion updated

--- client/src/components/BlurCircle.jsx
+++ client/src/components/BlurCircle.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
-const BlurCircle = () => {
+const BlurCircle = ({top = "auto", left = "auto", right = "auto", bottom = "auto"}) => {
   return (
-    <div>BlurCircle</div>
+    <div className='absolute -z-50 h-58 aspect-square
+    rounded-full bg-primary/30 blur-3xl'
+    style={{top: top, left: left, right: right, bottom: bottom }}>
+      
+    </div>
   )
 }
 
--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -1,6 +1,7 @@
 import { ArrowRight } from 'lucide-react'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import BlurCircle from './BlurCircle'
 
 const FeaturedSection = () => {
 
@@ -12,8 +13,9 @@ const FeaturedSection = () => {
 
         <div className='relative flex items-center justify-between pt-20
         pb-10'>
+            <BlurCircle top='0' right='-80px'/>
             <p className='text-gray-300 font-medium text-lg'>Not showing</p>
-            <button onClick={()=> navigate('/movies')} className='group flex items-center gap-2 text-sm text-gray-300'>
+            <button onClick={()=> navigate('/movies')} className='group flex items-center gap-2 text-sm text-gray-300 cursor-pointer'>
                 View All 
                 <ArrowRight className='group-hover:translate-x-0.5 transition w-4.5 h-4.5'/>
             </button>
@@ -21,7 +23,9 @@ const FeaturedSection = () => {
 
         <div></div>
         
-        <div></div>     
+        <div className='flex justify-center mt-20'>
+            <button className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer'>Show more</button>
+        </div>     
     </div>
   )
 }
