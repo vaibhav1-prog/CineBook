436e396dc5d2dbc5aa9ce1f0bf74f7d510d1b5a1
vaibhav1-prog <baishkhiyarv@gmail.com>
2026-05-18 15:45:31 +0530

    adding admin dashboard

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,9 +1,55 @@
+import { ChartLineIcon, CircleDollarSignIcon, PlayCircleIcon, UsersIcon } from 'lucide-react';
 import React from 'react'
+import { dummyDashboardData } from '../../assets/assets';
+import Loading from '../../components/Loading';
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
+
+    const currency = import.meta.env.VITE_CURRENCY
+
+    const [dashboardData, setDashboardData] = useState({
+      totalBookings: 0,
+      totalRevenue: 0,
+      activeShows: [],
+      totalUser: 0
+    });
+
+const [loading, setLoading] = useState(true);
+
+const dashboardCards = [
+  {
+    title: "Total Bookings",
+    value: dashboardData.totalBookings || "0",
+    icon: ChartLineIcon
+  },
+
+  {
+    title: "Total Revenue",value: dashboardData.totalRevenue || "0",icon: CircleDollarSignIcon
+  },
+
+  {
+    title: "Active Shows",value: dashboardData.activeShows.length || "0",icon: PlayCircleIcon
+  },
+
+  {
+    title: "Total Users",value: dashboardData.totalUser || "0",icon: UsersIcon
+  }
+]
+
+const fetchDashboardData = async () => {
+  setDashboardData(dummyDashboardData)
+  setLoading(false)
+};
+
+useEffect(() => {
+  fetchDashboardData();
+}, []);
+
+  return ! loading ? (
+  <div>
+
+  </div>
+) : <Loading />
 }
 
 export default Dashboard
